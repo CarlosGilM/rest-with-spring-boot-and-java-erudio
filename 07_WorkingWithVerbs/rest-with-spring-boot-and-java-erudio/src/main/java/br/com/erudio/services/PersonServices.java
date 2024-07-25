674594e8303be3f5868753566fb579506651f72a
Carlos Gil <carlosgilmds@gmail.com>
2024-07-25 12:02:05 -0300

    Working With Verbs

---
+++ 07_WorkingWithVerbs/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,62 @@
+package br.com.erudio.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.model.Person;
+
+@Service
+public class PersonServices {
+	private final AtomicLong counter = new AtomicLong();
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
+
+	public Person findById(String id) {
+		logger.info("Finding one person!");
+
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Leandro");
+		person.setLastName("Costa");
+		person.setAddress("Uberlândia - Minas Gerais - Brasil");
+		person.setGender("Male");
+		return person;
+	}
+
+	public Person Create(Person P) {
+		logger.info("Creating one person!");
+		return P;
+	}
+
+	public Person Update(Person P) {
+		logger.info("Updating one person!");
+		return P;
+	}
+
+	public void Delete(String id) {
+		logger.info("Deleting one person!");
+	}
+
+	private Person mockPerson(int i) {
+		logger.info("Finding all people!");
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name " + i);
+		person.setLastName("Last name " + i);
+		person.setAddress("Some address in Brasil " + i);
+		person.setGender("Male");
+		return person;
+	}
+}
